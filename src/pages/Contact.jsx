3d67118c30175737dd1caabed09cdf89bb581f59
kaliuser876 <jonrrich112@gmail.com>
2025-07-18 23:14:34 -0500

    Created 3 buttons that have the github, linkedin, and resume icons. Need to continue working on the resume so it will download my current resuem

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,26 @@
+import githubIconImage from '../assets/github-mark.svg';
+import linkedInIconImage from '../assets/linkedin.svg';
+import resumeIcon from '../assets/resume.svg';
+
+
 export default function Contact(){
-    return <h1>Contact</h1>
+    const openInNewTab = (url) => {
+      window.open(url, '_blank', 'noopener,noreferrer');
+    };
+    return( 
+        <div>
+            <h1>Contact</h1>
+            <div className='flex justify-center gap-4'>
+                <button onClick={() => openInNewTab('https://github.com/kaliuser876')}>
+                    <img src={resumeIcon} alt='resume icon' className='w-8 h-8' />
+                </button>
+                <button onClick={() => openInNewTab('https://github.com/kaliuser876')}>
+                    <img src={githubIconImage} alt='GitHub icon' className='w-8 h-8 scale-75' />
+                </button>
+                <button onClick={() => openInNewTab('https://www.linkedin.com/in/jonathan-richardson-621281269/')}>
+                    <img src={linkedInIconImage} alt='LinkedIn icon' className='w-8 h-8' />
+                </button>
+            </div>
+        </div>
+    )
 }
