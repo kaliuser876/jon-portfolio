b7a196786107fe093ddea37873864bf1156dfd23
kaliuser876 <jonrrich112@gmail.com>
2025-07-21 23:25:42 -0500

    Updated an image

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -2,6 +2,8 @@ import '../components/SkillIcon';
 import SkillIcon from '../components/SkillIcon';
 import TEST from '../assets/react.svg';
 
+
+
 export default function Skills(){
     return (
     <div>
@@ -10,7 +12,7 @@ export default function Skills(){
         </h1>
         <span className='flex justify-center gap-4 text-green-300 '>
             <SkillIcon src={TEST} display_text="React" />
-            <SkillIcon src={TEST} display_text="React" />
+            <SkillIcon src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg" display_text="React" />
             <SkillIcon src={TEST} display_text="React" />
         </span>
     </div>
